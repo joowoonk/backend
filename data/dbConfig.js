65503081fdf8af180a3457dcc172c8aad79487ca
MrR3set <bairon.pazrobles@gmail.com>
2020-04-28 12:27:27 -0700

    added db_ENV enviroment variable

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,4 +1,6 @@
 const knex = require("knex");
 const knexConfig = require("../knexfile");
 
-module.exports = knex(knexConfig.production);
+const enviroment = process.env.DB_ENV || "development";
+
+module.exports = knex(knexConfig[enviroment]);
