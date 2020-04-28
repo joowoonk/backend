a9a6dbe97620db7ae9f8d77a425a651dccf2b15e
MrR3set <bairon.pazrobles@gmail.com>
2020-04-28 14:27:52 -0700

    fixed path for migrations

--- knexfile.js
+++ knexfile.js
@@ -20,7 +20,7 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations'
+      directory: "./data/migrations"
     }
   }
 };
